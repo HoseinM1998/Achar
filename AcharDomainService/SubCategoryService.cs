aadd57b6620924d478bc3f2374e89667c0275b51
HoseinM1998 <hmoslehi98@gmail.com>
2025-02-15 20:35:20 +0330

    add subcategoryservice

---
+++ AcharDomainService/SubCategoryService.cs
@@ -0,0 +1,45 @@
+﻿using AcharDomainCore.Contracts.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcharDomainCore.Contracts.SubCategory;
+using AcharDomainCore.Dtos;
+using AcharDomainCore.Dtos.SubCategoryDto;
+using AcharDomainCore.Entites;
+
+namespace AcharDomainService
+{
+    public class SubCategoryService : ISubCategoryService
+    {
+        private readonly ISubCategoryRepository _repository;
+        public SubCategoryService(ISubCategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> CreateSubCategory(SubCategoryDto subCategoryDto, CancellationToken cancellationToken)
+            => await _repository.CreateSubCategory(subCategoryDto, cancellationToken);
+
+        public async Task<bool> UpdateSubCategory(SubCategoryDto subCategoryDto, CancellationToken cancellationToken)
+            => await _repository.UpdateSubCategory(subCategoryDto, cancellationToken);
+
+
+        public async Task<int> SubCategoryCount(CancellationToken cancellationToken)
+            => await _repository.SubCategoryCount( cancellationToken);
+
+
+        public async Task<SubCategory> GetSubCategoryById(int id, CancellationToken cancellationToken)
+            => await _repository.GetSubCategoryById(id, cancellationToken);
+
+
+        public async Task<List<SubCategory>> GetAllSubCategory(CancellationToken cancellationToken)
+            => await _repository.GetAllSubCategory(cancellationToken);
+
+
+        public async Task<bool> DeleteCategory(SoftDeleteDto delete, CancellationToken cancellationToken)
+            => await _repository.DeleteCategory(delete, cancellationToken);
+
+    }
+}
