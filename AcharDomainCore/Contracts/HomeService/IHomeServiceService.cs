120396dffac28b1509d0e87133bd7e6e09dbc90a
HoseinM1998 <hmoslehi98@gmail.com>
2025-02-15 20:28:03 +0330

    add homeservise service

--- AcharDomainCore/Contracts/HomeService/IHomeServiceService.cs
+++ AcharDomainCore/Contracts/HomeService/IHomeServiceService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AcharDomainCore.Dtos.HomeServiceDto;
+using AcharDomainCore.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace AcharDomainCore.Contracts.HomeService
 {
     public interface IHomeServiceService
     {
+        Task<int> CreateHomeService(HomeServiceDto homeService, CancellationToken cancellationToken);
+        Task<bool> UpdateHomeService(HomeServiceDto homeService, CancellationToken cancellationToken);
+        Task<int> HomeServiceCount(CancellationToken cancellationToken);
+        Task<Entites.HomeService> GetHomeServiceById(int id, CancellationToken cancellationToken);
+        Task<List<Entites.HomeService>> GetHomeServices(CancellationToken cancellationToken);
+        Task<bool> DeleteHomeService(SoftDeleteDto delete, CancellationToken cancellationToken);
     }
 }
---
+++ AcharDomainService/HomeServiceService.cs
@@ -0,0 +1,44 @@
+﻿using AcharDomainCore.Contracts.Comment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcharDomainCore.Contracts.HomeService;
+using AcharDomainCore.Dtos;
+using AcharDomainCore.Dtos.HomeServiceDto;
+
+namespace AcharDomainService
+{
+    public class HomeServiceService : IHomeServiceService
+    {
+        private readonly IHomeServiceRepository _repository;
+        public HomeServiceService(IHomeServiceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> CreateHomeService(HomeServiceDto homeService, CancellationToken cancellationToken)
+            => await _repository.CreateHomeService(homeService, cancellationToken);
+
+        public async Task<bool> UpdateHomeService(HomeServiceDto homeService, CancellationToken cancellationToken)
+            => await _repository.UpdateHomeService(homeService, cancellationToken);
+
+
+        public async Task<int> HomeServiceCount(CancellationToken cancellationToken)
+            => await _repository.HomeServiceCount(cancellationToken);
+
+
+        public async Task<AcharDomainCore.Entites.HomeService> GetHomeServiceById(int id, CancellationToken cancellationToken)
+            => await _repository.GetHomeServiceById(id,cancellationToken);
+
+
+        public async Task<List<AcharDomainCore.Entites.HomeService>> GetHomeServices(CancellationToken cancellationToken)
+            => await _repository.GetHomeServices(cancellationToken);
+
+
+        public async Task<bool> DeleteHomeService(SoftDeleteDto delete, CancellationToken cancellationToken)
+            => await _repository.DeleteHomeService(delete,cancellationToken);
+
+    }
+}
