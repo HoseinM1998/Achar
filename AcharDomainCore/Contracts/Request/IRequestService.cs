6e9f9074f17bf83a6cd72bca54b766dc1c0f330d
HoseinM1998 <hmoslehi98@gmail.com>
2025-02-15 20:31:59 +0330

    add requestservice

--- AcharDomainCore/Contracts/Request/IRequestService.cs
+++ AcharDomainCore/Contracts/Request/IRequestService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AcharDomainCore.Dtos.Request;
+using AcharDomainCore.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,12 @@ namespace AcharDomainCore.Contracts.Request
 {
     public interface IRequestService
     {
+        Task<int> CreateRequest(RequestDto requestDto, CancellationToken cancellationToken);
+        Task<bool> UpdateRequest(Entites.Request request, CancellationToken cancellationToken);
+        Task<int> RequestCount(CancellationToken cancellationToken);
+        Task<Entites.Request> GetRequestById(int id, CancellationToken cancellationToken);
+        Task<List<Entites.Request?>> GetRequests(CancellationToken cancellationToken);
+        Task<bool> DeleteRequest(SoftDeleteDto delete, CancellationToken cancellationToken);
+        public Task<bool> ChangeRequestStatus(StatusRequestDto newStatus, CancellationToken cancellationToken);
     }
 }
---
+++ AcharDomainService/RequestService.cs
@@ -0,0 +1,48 @@
+﻿using AcharDomainCore.Contracts.Comment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcharDomainCore.Contracts.Request;
+using AcharDomainCore.Dtos;
+using AcharDomainCore.Dtos.Request;
+using AcharDomainCore.Entites;
+
+namespace AcharDomainService
+{
+    public class RequestService : IRequestService
+    {
+        private readonly IRequestRepository _repository;
+        public RequestService(IRequestRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> CreateRequest(RequestDto requestDto, CancellationToken cancellationToken)
+            => await _repository.CreateRequest(requestDto,cancellationToken);
+
+        public async Task<bool> UpdateRequest(Request request, CancellationToken cancellationToken)
+            => await _repository.UpdateRequest(request, cancellationToken);
+
+
+        public async Task<int> RequestCount(CancellationToken cancellationToken)
+            => await _repository.RequestCount(cancellationToken);
+
+
+        public async Task<Request> GetRequestById(int id, CancellationToken cancellationToken)
+            => await _repository.GetRequestById(id, cancellationToken);
+
+        public async Task<List<Request?>> GetRequests(CancellationToken cancellationToken)
+            => await _repository.GetRequests( cancellationToken);
+
+
+        public async Task<bool> DeleteRequest(SoftDeleteDto delete, CancellationToken cancellationToken)
+            => await _repository.DeleteRequest(delete, cancellationToken);
+
+
+        public async Task<bool> ChangeRequestStatus(StatusRequestDto newStatus, CancellationToken cancellationToken)
+            => await _repository.ChangeRequestStatus(newStatus, cancellationToken);
+
+    }
+}
