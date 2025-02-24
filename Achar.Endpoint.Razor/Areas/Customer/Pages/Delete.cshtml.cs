9eb57b08cc9da811027ef0aa68238cde60a6fedf
HoseinM1998 <hmoslehi98@gmail.com>
2025-02-25 00:53:30 +0330

    add delete customer endpoint

--- Achar.Endpoint.Razor/Areas/Customer/Pages/Delete.cshtml.cs
+++ Achar.Endpoint.Razor/Areas/Customer/Pages/Delete.cshtml.cs
@@ -1,12 +1,43 @@
+using AcharDomainCore.Contracts.City;
+using AcharDomainCore.Contracts.Customer;
+using AcharDomainCore.Dtos;
+using AcharDomainCore.Dtos.CustomerDto;
+using AcharDomainCore.Entites;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Achar.Endpoint.Razor.Areas.Customer.Pages
 {
+    [Authorize(Roles = "Customer")]
+
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+
+        private readonly ICustomerAppService _customerAppServices;
+
+        public DeleteModel(ICustomerAppService customerAppServices, ICityAppService cityAppService)
         {
+            _customerAppServices = customerAppServices;
+        }
+
+        public async Task OnGet(CancellationToken cancellationToken)
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostDelete( CancellationToken cancellationToken)
+        {
+            var userCustomerId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == "userCustomerId").Value);
+            if (ModelState.IsValid)
+            {
+                
+                await _customerAppServices.DeleteCustomer(userCustomerId, cancellationToken);
+                return LocalRedirect("/Account/Login");
+
+
+            }
+            return RedirectToPage("/Index");
         }
     }
 }
