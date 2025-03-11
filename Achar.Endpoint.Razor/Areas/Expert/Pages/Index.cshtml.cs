bc43712f34d82f42923f2d77bef4e33fad9622e2
HoseinM1998 <hmoslehi98@gmail.com>
2025-03-11 22:19:12 +0330

    add comment ui expert

--- Achar.Endpoint.Razor/Areas/Expert/Pages/Index.cshtml.cs
+++ Achar.Endpoint.Razor/Areas/Expert/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
+using AcharDomainCore.Contracts.Comment;
 using AcharDomainCore.Contracts.Customer;
 using AcharDomainCore.Contracts.Expert;
+using AcharDomainCore.Dtos.CommentDto;
 using AcharDomainCore.Dtos.CustomerDto;
 using AcharDomainCore.Dtos.ExpertDto;
 using Microsoft.AspNetCore.Authorization;
@@ -13,13 +15,21 @@ namespace Achar.Endpoint.Razor.Areas.Expert.Pages
     public class IndexModel : PageModel
     {
         private readonly IExpertAppService _expertAppService;
-        public IndexModel(IExpertAppService expertAppService)
+        private readonly ICommentAppService _commentAppService;
+
+        public IndexModel(IExpertAppService expertAppService, ICommentAppService commentAppService)
         {
             _expertAppService = expertAppService;
+            _commentAppService = commentAppService;
         }
 
         [BindProperty]
         public ExpertProfDto Expert { get; set; }
+
+        [BindProperty]
+        public List<GetCommentDto?> Comments { get; set; }
+
+
         public async Task OnGet(CancellationToken cancellationToken)
         {
             var userId = int.Parse(User.Claims.FirstOrDefault(u => u.Type == "userExpertId").Value);
@@ -28,6 +38,10 @@ namespace Achar.Endpoint.Razor.Areas.Expert.Pages
             //var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             //int userId = int.Parse(userIdClaim);
             Expert = await _expertAppService.GetExpertById(userId, cancellationToken);
+            Comments = await _commentAppService.GetCommentsByExpertId(userId, cancellationToken);
+
+
+
         }
     }
 }
